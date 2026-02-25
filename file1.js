9240314df3ee0b91939d27c4e70fbf1b6d857f68
minecraft0036 <aosi90736@gmail.com>
2026-02-25 21:05:35 +0900

    Add file1 (JS/TS), update index.html, add deps
    Add new files file1.js and file1.ts (click handler using a sleep Promise) and wire file1.js into index.html. Update index.html layout, nav styling, content blocks and Prof panel sizing. Add tsconfig.json and a large set of node_modules (ESLint-related packages and dependencies). Update Visual Studio workspace/solution metadata (.vs/*) including DocumentLayout and workspace state.

---
+++ file1.js
@@ -0,0 +1,5 @@
+const sleep = waitTime => new Promise(resolve => setTimeout(resolve, waitTime));
+let d = document.getElementById("sd");
+var df = document.getElementById("hide");
+df.addEventListener("click", function () { df.style.backgroundColor = "blue"; sleep(2000); df.style.backgroundColor = "forestgreem"; });
+d.textContent = "dsdfasf";
---
+++ file1.ts
@@ -0,0 +1,6 @@
+﻿const sleep = waitTime => new Promise(resolve => setTimeout(resolve, waitTime));
+let d:HTMLElement = document.getElementById("sd");
+var df = document.getElementById("hide")
+df.addEventListener("click", function () { df.style.backgroundColor = "blue"; sleep(2000);df.style.backgroundColor="forestgreem" })
+d.textContent = "dsdfasf";
+
